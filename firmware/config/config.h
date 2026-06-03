6591c8d2ae1b29e0fa1836c0effbdf30d051f3df
ShivamMathtech <drshivam49@gmail.com>
2026-06-03 17:06:03 +0530

    ready with configuration

---
+++ firmware/config/config.h
@@ -0,0 +1,15 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define SEALEVELPRESSURE_HPA 1013.25
+
+#define MQ135_PIN 34
+#define RAIN_PIN 35
+#define UV_PIN 32
+
+#define GPS_RX 16
+#define GPS_TX 17
+
+#define SD_CS 5
+
+#endif
---
+++ firmware/config/secrets.h
@@ -0,0 +1,10 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+const char* WIFI_SSID = "YOUR_WIFI";
+const char* WIFI_PASSWORD = "PASSWORD";
+
+const char* MQTT_SERVER = "broker.hivemq.com";
+const int MQTT_PORT = 1883;
+
+#endif
